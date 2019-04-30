0a02c88d29979ce9ef7ce7bccf23c1f9a9a97f3e
kuwingto <kuwingto@gmail.com>
2019-04-30 17:28:45 +0800

    enhance script

--- generatePNGnDEPTH_blender.py
+++ generatePNGnDEPTH_blender.py
@@ -1,14 +1,56 @@
 import bpy
+from bpy import context
+scene = context.scene
+##meter in unit
+#################parameter################################
+objectPath = "F:\\ronaldwork\\testData\\cadModel\\VShapePartsTemplate.STL"
+scalingFactor = 0.001
 
-#################SELECT################################
-bpy.data.objects['VShapePartsTemplate'].select = True
-
-
-#################SELECT################################
 namePrefix = "v"
 basePath = "F:\\ronaldwork\\testData\\images"
+
+#################CLEAR################################
+for obj in bpy.data.objects:
+    obj.select = True
+bpy.ops.object.delete()
+#################CAMERA################################
+for obj in bpy.data.objects:
+    obj.select = False
+    
+bpy.ops.object.camera_add()
+bpy.data.objects['Camera'].select = True
+scene.camera = context.object
+bpy.context.active_object.rotation_mode = 'XYZ'
+bpy.context.active_object.rotation_euler = (0,0 , 0)
+bpy.context.active_object.location[0] = 0
+bpy.context.active_object.location[1] = 0
+bpy.context.active_object.location[2] = 1
+#################LIGHT################################
+for obj in bpy.data.objects:
+    obj.select = False
+    
+bpy.ops.object.lamp_add(type='SUN')
+bpy.data.objects['Sun'].select = True
+bpy.context.active_object.rotation_mode = 'XYZ'
+bpy.context.active_object.rotation_euler = (0,0 , 0)
+bpy.context.active_object.location[0] = 0
+bpy.context.active_object.location[1] = 0
+bpy.context.active_object.location[2] = 1
+
+#################TEMPLATE################################
+bpy.ops.import_mesh.stl(filepath=objectPath)
+for obj in bpy.data.objects:
+    obj.select = False
+bpy.data.objects['VShapePartsTemplate'].select = True
+bpy.context.active_object.location[0] = 0
+bpy.context.active_object.location[1] = 0
+bpy.context.active_object.location[2] = 0
+bpy.context.active_object.scale[0] = scalingFactor
+bpy.context.active_object.scale[1] = scalingFactor
+bpy.context.active_object.scale[2] = scalingFactor
+
 ###################create rotation steps#######################################
-angleStep = 15
+angleStep = 10
 numofAlphaStep = int(360/angleStep)
 numofBetaStep = 1  #int(180/angleStep)
 numofGammaStep = 1# int(360/angleStep)
